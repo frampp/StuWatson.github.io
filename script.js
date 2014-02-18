661b5be30e88ddd47ed44925399f9a735f5bb060
Stu Watson <stuart.watson@appsbroker.com>
2014-02-18 09:40:20 +0000

    gadfs

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ var hitCheck = function(cell) {
 
 var survivalUpdate = function(cell) {
 	console.log("Fucked")
-	alert("Game over @ "+cell.x " " + cell.y)
+	alert("Game over @ "+cell.x + " " + cell.y)
 };
 
 var timerUpdate = function () {
