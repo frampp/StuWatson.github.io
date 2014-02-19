4359a7f74106597bd6cfcf33d60c00ab5c69909a
StuWatson <swatson314159@gmail.com>
2014-02-19 21:27:43 +0000

    Fixing events bug

--- Controllers/events.js
+++ Controllers/events.js
@@ -1,12 +1,10 @@
 (function () { birdApp.input = window.touchstart ? "touchstart" : "click" }());
 
 birdApp.removeEventListeners = function() {
-	birdApp.canvas.removeEventListener(birdApp.input, birdApp.continueTouch);
-	birdApp.canvas.removeEventListener(birdApp.input, birdApp.continueTouch);
+	birdApp.canvas.removeEventListener(birdApp.input, birdApp.continueTouch, false);
 	birdApp.canvas.removeEventListener(birdApp.input, birdApp.onTouch, false);
-	birdApp.canvas.removeEventListener(birdApp.input, birdApp.onTouch);
 	birdApp.canvas.removeEventListener(birdApp.input, birdApp.menuTouch, false);
-	birdApp.canvas.removeEventListener(birdApp.input, birdApp.menuTouch);
+
 };
 
 birdApp.clearTimeouts = function() {
@@ -23,5 +21,4 @@ birdApp.timerUpdate = function () {
 birdApp.addEventListeners = function (listener) {
 	birdApp.removeEventListeners();
 	birdApp.canvas.addEventListener(birdApp.input, listener, false);
-	birdApp.canvas.addEventListener(birdApp.input, listener);
 };
